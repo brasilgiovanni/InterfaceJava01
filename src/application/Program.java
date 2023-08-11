2c2739bbd485fd0d3fcee86c47bb3655975e1695
Giovanni Brasil <giovannisbrasil@gmail.com>
2023-08-11 11:51:57 -0300

    Version 02 funcionando

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,6 +7,8 @@ import java.util.Scanner;
 
 import model.entitites.CarRental;
 import model.entitites.Vehicle;
+import model.services.BrazilTaxService;
+import model.services.RentalService;
 
 public class Program {
 
@@ -20,11 +22,22 @@ public class Program {
 		
 		System.out.println("Entre com os dados do aluguel");
 		System.out.print("Modelo do carro: "); String carModel = sc.nextLine();
-		System.out.println("Retirada (dd/MM/yyyy hh:mm): "); LocalDateTime retirada = LocalDateTime.parse(sc.nextLine(), fmt);
-		System.out.println("Devolução (dd/MM/yyyy hh:mm): "); LocalDateTime devolucao = LocalDateTime.parse(sc.nextLine(), fmt);
+		System.out.print("Retirada (dd/MM/yyyy hh:mm): "); LocalDateTime retirada = LocalDateTime.parse(sc.nextLine(), fmt);
+		System.out.print("Devolução (dd/MM/yyyy hh:mm): "); LocalDateTime devolucao = LocalDateTime.parse(sc.nextLine(), fmt);
 		
 		CarRental car01 = new CarRental(retirada, devolucao,  new Vehicle(carModel));
 		
+		System.out.print("Entre com o preço por hora: "); double pricePerHour = sc.nextDouble();
+		System.out.print("Enre com o preço por dia: "); double pricePerDay = sc.nextDouble();
+		
+		RentalService rentalService = new RentalService(pricePerHour, pricePerDay, new BrazilTaxService());
+		
+		rentalService.processInvoice(car01);
+		
+		System.out.println("FATURA: ");
+		System.out.println("Pagamento Básico: " + car01.getInvoice().getBasicPayment());
+		System.out.println("Imposto: " + car01.getInvoice().getTax());
+		System.out.println("Pagamento total: " + car01.getInvoice().getTotalPayment());
 		
 		
 		
